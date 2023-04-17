884bfa420ffecc1ca46823e1928db8e3d794217b
Ifycode <marychord@gmail.com>
2023-04-17 05:36:59 +0100

    Create simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'order-app';
+export class AppComponent implements OnInit{
+  // @ts-ignore
+  form: FormGroup;
+
+  ngOnInit(): void {
+    this.form = this.createForm();
+  }
+
+  createForm() {
+    return new FormGroup({
+      name: new FormControl(''),
+      white_wine: new FormControl(0),
+      red_wine: new FormControl(0),
+      beer: new FormControl(0)
+    });
+  }
+
 }
